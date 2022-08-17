f101cef961e1899cca78bf38007bddff7d1e2635
lexNwimue <nwimueprogress@gmail.com>
2022-08-17 21:15:17 +0100

    updated environment variables

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connect to MongoDB and start server aftwards
-const dbURI = `mongodb+srv://lexNwimue:Kaycee<3@cluster0.bmtc1.mongodb.net/lendsqrDB?retryWrites=true&w=majority`;
+const dbURI = `mongodb+srv://lexNwimue:${process.env.password}@cluster0.bmtc1.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
@@ -20,6 +20,7 @@ mongoose
   })
   .catch((err) => console.log(err));
 
+app.get("/", (req, res) => res.json({ welcome: "Hi, welcome" }));
 app.post("/signup", signup);
 app.post("/deposit", deposit);
 app.post("/transfer", transfer);
